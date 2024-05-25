ec716a4a372601e85a84c3392d14de0578dc455e
Anton <157398457+Anton199621@users.noreply.github.com>
2024-05-25 15:44:11 +0700

    Delete src directory

--- src/main/java/constants/EnvConfig.java
+++
@@ -1,7 +0,0 @@
-package configure;
-
-public class EnvConfig {
-    public static final String COURIER_PATH = "/api/v1/courier";
-    public static final String ORDER_PATH = "/api/v1/orders";
-    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
-}
